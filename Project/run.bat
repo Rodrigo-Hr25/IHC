68a5c3bb982db5ef428fa43d86cda9e352be9dbb
hramos25 <rodrigomiguel471@gmail.com>
2025-04-10 12:17:27 +0100

    Debug mode ON

--- Project/run.bat
+++ Project/run.bat
@@ -6,6 +6,7 @@ set "PORT=%~2"
 
 set "FLASK_APP=%APP%/__init__.py"
 set "FLASK_RUN_PORT=%PORT%"
+set "FLASK_DEBUG=1"
 
 flask run
 
--- Project/run.sh
+++ Project/run.sh
@@ -5,4 +5,5 @@ PORT="$2"
 
 export FLASK_APP=$APP/__init__.py
 export FLASK_RUN_PORT=$PORT
+export FLASK_DEBUG=1
 flask run
